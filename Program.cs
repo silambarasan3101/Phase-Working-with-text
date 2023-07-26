1f2e8c5370c07b94c0667900a606801c104eddf3
silambarasan3101 <139097561+silambarasan3101@users.noreply.github.com>
2023-07-26 16:19:30 +0530

    Program.cs

--- Program.cs
+++ Program.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace Phase_Working_with_Text
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+     static void Main(string[] args)
         {
+
+            doApp();
+        }
+
+        public static void doApp()
+        {
+            string mary = "Mary had a little lamb.";
+            Console.WriteLine("Original string=" + mary);
+            Console.WriteLine("Clone of a string =" + mary.Clone());
+            Console.WriteLine("Compare two strings =" + mary.CompareTo("Mary had a big lamb"));
+            Console.WriteLine("Characters in a string =" + mary.Contains("r"));
+            Console.WriteLine("String ending with =" + mary.EndsWith("lamb."));
+            Console.WriteLine("All lowercase =" + mary.ToLower());
+            Console.WriteLine("All uppercase =" + mary.ToUpper());
+            Console.WriteLine("Insert string within a string =" + mary.Insert(4, " and Jack "));
+            Console.WriteLine("Splitting string into words:");
+            string[] parts = mary.Split();
+            foreach (string p in parts)
+            {
+                Console.WriteLine(p);
+            }
+
+            StringBuilder sb = new StringBuilder("");
+            for (int i = 0; i < 10; i++)
+            {
+                sb.Append(i.ToString() + " ");
+            }
+            Console.WriteLine(sb.ToString());
+            Console.ReadKey();
+
         }
     }
 }
